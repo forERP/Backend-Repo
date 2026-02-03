c71b5b495f62249b449b13eea11316edba96d497
bluesky <bluesky@example.com>
2026-02-03 15:41:25 +0900

    feat: JWT 인증 시스템 구현

--- src/main/java/com/forerp/erp/attendance/domain/Attendance.java
+++ src/main/java/com/forerp/erp/attendance/domain/Attendance.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -23,6 +24,9 @@ public class Attendance{
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Column(name = "work_date", nullable = false)
+    private LocalDate workDate;
+
     @Column(name = "clock_in", nullable = false)
     private LocalDateTime clockIn; // 출근 시각
 
@@ -33,17 +37,27 @@ public class Attendance{
     @Column(nullable = false)
     private AttendanceStatus status;
 
-    // 출근
-    @Builder
-    public Attendance(LocalDateTime clockIn, User user){
+    // 출근 또는 결근 기록(자동)
+    @Builder(builderMethodName = "clockInBuilder")
+    public Attendance(LocalDateTime clockIn, User user, LocalDate workDate){
         this.user = user;
+        this.workDate = workDate;
         this.clockIn = clockIn;
-        this.status = AttendanceStatus.WORK;
+        this.status = (clockIn != null) ? AttendanceStatus.WORK : AttendanceStatus.ABSENT;
     }
 
-    // 퇴근
-    public void recordClockOut(){
+    // 휴가 기록(관리자가 휴가를 등록 할 떄 사용)
+    @Builder(builderMethodName = "leaveRecordBuilder")
+    public Attendance(User user, LocalDate workDate){
+        this.user = user;
+        this.workDate = workDate;
+        this.status = AttendanceStatus.LEAVE;
+        this.clockIn = null; // 수동 기록시 출근 시간은 없음
+        this.clockOut = null; // 수동 기록시 퇴근 시간은 없음
+    }
 
+    // 퇴근 시간 기록
+    public void recordClockOut(){
         this.clockOut = LocalDateTime.now();
     }
 
