058c307cc202be9b613c3472586fc81b17e7dc16
bluesky <bluesky@example.com>
2026-02-11 09:45:29 +0900

    feat:config-webConfig추가

--- src/main/java/com/forerp/erp/common/bootstrap/AdminBootstrapProperties.java
+++ src/main/java/com/forerp/erp/common/bootstrap/AdminBootstrapProperties.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "app.bootstrap.admin")
--- src/main/java/com/forerp/erp/config/SecurityConfig.java
+++ src/main/java/com/forerp/erp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.forerp.erp.common.jwt.JwtSecurityFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,13 +28,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable); // csrf 보호 비활성화(토큰 방식을 사용하므로)
+        http.csrf(AbstractHttpConfigurer::disable);// csrf 보호 비활성화(토큰 방식을 사용하므로)
         http.formLogin(AbstractHttpConfigurer::disable); // 기본 로그인 폼 및 HTTP basic 인증 비활성화(우리가 만든 로그인 API 사용)
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션 관리 정책을 STATELESS로 설정(서버가 사용자 상태를 저장하지 않음)
+        http.cors(Customizer.withDefaults()); // WebConfig의 설정을 적용
+
         // API 경로별 접근 권한 설정
         http.authorizeHttpRequests(auth -> auth
                         // 로그인 API와 초기 사용자 설정 API는 토큰 없이도 접근 허용
-                        .requestMatchers("/api/users/login", "/api/users/setup/**", "/api/attendance/**").permitAll()
+                        .requestMatchers("/api/users/login", "/api/users/login/pos", "/api/users/setup/**", "/api/attendance/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
